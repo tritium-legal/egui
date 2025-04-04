5a962efb4df544699f4a6bf23402b17ca099d497
Drew <drew@tritium.legal>
2025-04-04 11:20:30 +0100

    improve page text rendering

--- crates/egui_glow/src/painter.rs
+++ crates/egui_glow/src/painter.rs
@@ -542,7 +542,7 @@ impl Painter {
                 let data: Vec<u8> = {
                     profiling::scope!("font -> sRGBA");
                     image
-                        .srgba_pixels(None)
+                        .srgba_pixels(Some(0.9)) // critical for avoiding artifacts in rendered docs
                         .flat_map(|a| a.to_array())
                         .collect()
                 };
